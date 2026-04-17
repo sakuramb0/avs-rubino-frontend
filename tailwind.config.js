f8becfaf2d12be01e861ac34f90143e3a50734fc
Stefano Orlando <eorlste@ericsson.com>
2026-04-17 16:42:14 +0200

    fix: ESM syntax for Vite build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default /** @type {import('tailwindcss').Config} */
+{
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
